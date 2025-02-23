cac9956d4250105f2ee9083a02747211514c4a30
kang <connor.r.kelleher@gmail.com>
2025-02-23 16:42:58 -0500

    delete code

--- debug.c
+++ debug.c
@@ -9,31 +9,12 @@ void print_bytes(unsigned char *b, uint16_t length) {
     }
 }
 
-void dump_constant_pool(uint16_t count, constant_pool_t *cp) {
+void dump_constant_pool(class_file_t *cf, uint16_t count, constant_pool_t *cp) {
     printf("Constant Pool : %u\n", count - 1);
     for (uint16_t i = 0; i < count - 1; i++) {
         printf("  #%02u = ", i + 1);
-        switch (cp[i].tag) {
-            case CONSTANT_UTF8:
-                printf("%-20s ", "Utf8");
-                // FIXME - should probably do something if this string is long like a "..."
-                printf("%s ", cp[i].info.utf8_info.bytes);
-                break;
-            case CONSTANT_CLASS:
-                printf("%-20s ", "Class");
-                printf("#%d", cp[i].info.class_info.name_index);
-                break;
-            case CONSTANT_METHOD_REF:
-                printf("%-20s ", "Methodref");
-                printf("#%d.#%d", cp[i].info.method_ref_info.class_index, cp[i].info.method_ref_info.name_and_type_index);
-                break;
-            case CONSTANT_NAME_AND_TYPE:
-                printf("%-20s ", "NameAndType");
-                printf("#%d:#%d", cp[i].info.name_and_type_info.name_index, cp[i].info.name_and_type_info.descriptor_index);
-                break;
-            default:
-                printf("Unknown tag 0x%02d", cp[i].tag);
-        }
+        printf("%-18s", get_constant_tag_name(cp[i].tag));
+        print_constant_info_at(cf, i + 1);
         printf("\n");
     }
 }
@@ -312,7 +293,7 @@ void dump_classfile(class_file_t *class_file) {
     printf("%-25s 0x%08x '\n", "Magic Number", class_file->magic);
     printf("%-25s %4d '\n", "Minor Version", class_file->minor_version);
     printf("%-25s %4d '\n", "Major Version", class_file->major_version);
-    dump_constant_pool(class_file->constant_pool_count, class_file->constant_pool);
+    dump_constant_pool(class_file, class_file->constant_pool_count, class_file->constant_pool);
     printf("%-25s 0x%04x '\n", "Access Flags", class_file->access_flags);
     printf("%-25s 0x%04x // %s'\n", "This class", class_file->this_class, get_classname(class_file, class_file->this_class));
     printf("%-25s 0x%04x '\n", "Super class", class_file->super_class);
